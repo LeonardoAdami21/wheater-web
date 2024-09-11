4b38c239c012bd3e908cf40355c5d5a83296be5e
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-09-11 13:45:30 -0300

    feat/weather, componente weather implementado com sucesso.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 import "./App.css";
 import React, { useState } from "react";
 import Search from "./components/Search";
+import Weather from "./components/Weather";
+import axios from "axios";
+import { reactWeatherApiKey, reactWeatherApiUrl } from "./env/envoriment";
 
 const App = () => {
   const [city, setCity] = useState("");
-  const [weather, setWeather] = useState({});
-  const [error, setError] = useState(false);
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchWeather = async () => {
+    try {
+      const response = await axios
+        .get(
+          `${reactWeatherApiUrl}?q=${city}&appid=${reactWeatherApiKey}&units=metric`,
+        )
+        .then((response) => {
+          setWeather(response.data);
+          setCity("");
+          setError(null);
+        });
+      return response;
+    } catch (error) {
+      setError("City not found");
+      throw new Error(error);
+    }
+  };
+
   return (
     <div className="app">
-      <h1>Wheater Web</h1>
-      <Search city={city} setCity={setCity} />
+      <h1>Weather Web</h1>
+      <Search city={city} setCity={setCity} fetchWeather={fetchWeather} />
       {error && <div className="error">{error}</div>}
+      {weather && <Weather weather={weather} />}
     </div>
   );
 };
---
+++ src/env/envoriment.js
@@ -0,0 +1,2 @@
+export const reactWeatherApiKey = process.env.REACT_APP_OPEN_WEATHER_KEY;
+export const reactWeatherApiUrl = process.env.REACT_APP_OPEN_WEATHER_URL;
