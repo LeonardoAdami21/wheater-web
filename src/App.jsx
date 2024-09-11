e36aec9cb0f3d3afe07150118949326d117849a7
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-09-11 10:55:47 -0300

    feat/configurations, configurações e dependencias instaladas para o funcionamento do front

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Wheater Web</h1>
+    </div>
+  );
+};
+
+export default App;
